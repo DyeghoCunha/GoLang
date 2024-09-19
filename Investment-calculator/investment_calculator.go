912a959655f0bac46d6316eb7ac86b5b94440f12
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2024-09-19 18:21:31 -0300

    estudo

--- Investment-calculator/investment_calculator.go
+++ Investment-calculator/investment_calculator.go
@@ -4,11 +4,23 @@ import "math"
 import "fmt"
 
 func main() {
-	var investmentAmount, years = 1000, 10
-	expectedReturnRate := 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	const inflationRate = 6.5
+	var investmentAmount float64
+	var years float64
+	var expectedReturnRate float64
 
-	fmt.Print(futureValue)
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected Return: ")			
+	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 
 }
---
+++ Investment-calculator/profit_calculator.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+import "math"
+
+func profitCalculator(){
+
+var revenue float64
+var expenses float64
+var taxRate float64
+var earningsBeforeTax float64
+var earningsAfterTax float64
+var ratio float64
+
+
+fmt.Print("What is the revenue: ")
+fmt.Scan(&revenue)
+fmt.Print("What is the Expenses: ")
+fmt.Scan(&expenses)
+fmt.Print("What is the Tax Rate: ")
+fmt.Scan(&taxRate)
+
+
+earningsBeforeTax = revenue - expenses
+earningsAfterTax = earningsBeforeTax - (earningsBeforeTax*(taxRate/100))
+ratio = earningsBeforeTax/earningsAfterTax
+fmt.Print("You Earnings Before Tax are: ")
+fmt.Println(earningsBeforeTax)
+
+fmt.Print("Your Earnings After Tax are: ")
+fmt.Println(earningsAfterTax)
+
+fmt.Print("Your Tax Ratio is: ")
+fmt.Println(ratio)
+
+}
