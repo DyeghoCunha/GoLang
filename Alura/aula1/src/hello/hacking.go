754423701982697618d87148503c1763514ea37b
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2025-03-17 18:07:28 -0300

    inicio de estudo

--- Alura/aula1/src/hello/hacking.go
+++ Alura/aula1/src/hello/hacking.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
-import "os"
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 	exibeIntroducao()
+	leSitesDoArquivo()
 
 	for {
 		exibeMenu()
@@ -52,14 +60,59 @@ func leComando() int {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites := []string{"https://www.alura.com.br",
-		"https://random-status-code.herokuapp.com/",
-		"https://www.uol.com.br",
-		"https://www.caelum.com.br"}
-
-	for _, site := range sites {
-		fmt.Println(site)
-		resp, _ := http.Get(site)
-		fmt.Println(resp.Status)
+	//	sites := []string{"https://www.alura.com.br",
+	//	"https://random-status-code.herokuapp.com/",
+	//	"https://www.uol.com.br",
+	//	"https://www.caelum.com.br"}
+	sites := leSitesDoArquivo()
+	for i := 0; i < monitoramentos; i++ {
+		for _, site := range sites {
+			//fmt.Println(site)
+			testaSite(site)
+		}
+		time.Sleep(delay * time.Second)
+		fmt.Println("-------------------------------------\n")
 	}
+}
+func testaSite(site string) {
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	switch resp.StatusCode {
+	case 200:
+		fmt.Println("Site", site, "foi carregado com sucesso")
+	default:
+		fmt.Println("Site", site, "foi carregado com falha. Status Code:", resp.StatusCode)
+	}
+}
+
+func leSitesDoArquivo() []string {
+	var sites []string
+	//	arquivo, err := os.Open("sites.txt")
+	//	arquivo, err := ioutil.ReadFile("C:\\Users\\Dyegho\\Documents\\GitHub\\GoLang\\Alura\\aula1\\src\\hello\\sites.txt")
+
+	arquivo, err := os.Open("C:\\Users\\Dyegho\\Documents\\GitHub\\GoLang\\Alura\\aula1\\src\\hello\\sites.txt")
+
+	if err != nil {
+		fmt.Println("Erro ao abrir o arquivo", err)
+	}
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		//fmt.Println(linha)
+		sites = append(sites, linha)
+
+		if err == io.EOF {
+
+			break
+		}
+
+	}
+	fmt.Println(sites)
+	return sites
 }
