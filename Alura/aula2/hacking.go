9a66c6202c2495590d6b811c739121d210cda53c
Dyegho M C G Cunha <dyeghocunha@gmail.com>
2025-03-31 08:03:06 -0300

    estudo

--- Alura/aula2/hacking.go
+++ Alura/aula2/hacking.go
@@ -12,22 +12,33 @@ type ContaCorrente struct {
 }
 
 func main() {
-	var contaDoGuilherme ContaCorrente = ContaCorrente{
-		titular:       "Guilherme",
-		numeroAgencia: 589,
-		numeroConta:   123456,
-		saldo:         125.5,
-	}
-	contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
-	fmt.Println(contaDoGuilherme, contaDaBruna)
+	//var contaDoGuilherme ContaCorrente = ContaCorrente{
+	//	titular:       "Guilherme",
+	//	numeroAgencia: 589,
+	//	numeroConta:   123456,
+	//	saldo:         125.5,
+	//}
 
+	//contaDaBruna := ContaCorrente{"Bruna", 222, 111222, 200}
+	//fmt.Println(contaDoGuilherme, contaDaBruna)g
+	//
 	var contaDaCris *ContaCorrente
 	contaDaCris = new(ContaCorrente)
 	contaDaCris.titular = "Cris"
-
-	contaDaValter := &ContaCorrente{}
-	contaDaValter.titular = "Valter"
-
-	fmt.Println(contaDaValter)
+	var contaDaCris2 *ContaCorrente
+	contaDaCris2 = new(ContaCorrente)
+	contaDaCris2.titular = "Cris"
+	fmt.Println(contaDaCris == contaDaCris2)
+	fmt.Println(contaDaCris)
+	fmt.Println(contaDaCris2)
+	fmt.Println(*contaDaCris == *contaDaCris2)
+	fmt.Println(&contaDaCris)
+	fmt.Println(&contaDaCris2)
+	fmt.Println(&contaDaCris == &contaDaCris2)
+	//
+	//contaDaValter := &ContaCorrente{}
+	//contaDaValter.titular = "Valter"
+	//
+	//fmt.Println(contaDaValter)
 
 }
